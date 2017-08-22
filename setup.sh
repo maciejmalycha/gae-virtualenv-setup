810abbc33ae1461fc47f3a3f3ab85622e5c62dce
Maciej Malycha <maciej.malycha@genesyslab.com>
2017-08-22 16:36:20 +0200

    Fixed install script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-echo "Attempting install: gaecustomize.py to $VIRTUAL_ENV/lib/python2.7/site-packages/gaecustomize.py"
-cp gaecustomize.py $VIRTUAL_ENV/lib/python2.7/site-packages/
-
 echo "Attempting install: gae.pth to $VIRTUAL_ENV/lib/python2.7/site-packages/gae.pth"
 export apparent_sdk_dir
 cat gae.pth | envsubst > $VIRTUAL_ENV/lib/python2.7/site-packages/gae.pth
